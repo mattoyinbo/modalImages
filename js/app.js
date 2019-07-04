13403529693ab0b26b938e2421db49a9c4eb7c9e
mattoyinbo <mattoyinbo@gmail,com>
2019-07-03 22:33:15 -0400

    Updated the modal func in app.js to enlarge images

--- js/app.js
+++ js/app.js
@@ -1,48 +1,77 @@
 (function() {
-  let imageList = [];
-  let counter = 0;
-  const images = document.querySelectorAll(".store-img");
-  const container = document.querySelector(".lightbox-container");
-  const item = document.querySelector(".lightbox-item");
-  const closeIcon = document.querySelector(".lightbox-close");
-  const btnLeft = document.querySelector(".btnLeft");
-  const btnRight = document.querySelector(".btnRight");
-  // add all images to the modal
-  images.forEach(function(img) {
+   //lightbox
+
+//All Images 
+let imageList = [];
+//let fruits = ['apple','mango','grape'];
+let counter = 0;
+
+    const images = document.querySelectorAll('.card-img-top');
+    const container = document.querySelector('.lightbox-container');
+    const  item = document.querySelector('.lightbox-item');
+    const  closeModal = document.querySelectorAll('.lightbox-close');
+    //const  control = document.querySelectorAll('.lightbox-control');
+    const  btnLeft = document.querySelector('.btnLeft');
+    const  btnRight = document.querySelector('.btnRight');
+    let imageSrc;
+
+ //add all images to ImageList arr
+images.forEach((img)=>{
     imageList.push(img.src);
-  });
 
-  //add event listener to open modal and show image
-  images.forEach(function(img) {
-    img.addEventListener("click", function(event) {
-      // show modal
-      container.classList.add("show");
-      // get source
-      let src = event.target.src;
-      // change counter
-      counter = imageList.indexOf(src);
+});
 
-      // show modal with image
-      item.style.backgroundImage = `url(${src})`;
+//console.log(imageList);
+    
+//console.log(images[0]);
+    
+//Open Modal
+images.forEach( (img)=>{
+
+        img.addEventListener('click',(event)=> {
+            //Get clicked image source
+            imageSrc = event.target.src;
+            //show lightbox modal on click event
+            container.classList.add("show");
+            
+            //set modal background to clicked image
+            item.style.backgroundImage = `url('${imageSrc}')`;  
+            //counter = imageList.findIndex((index)=>{return index == imageSrc;});
+           //console.log(counter);
+           counter = imageList.indexOf(imageSrc); 
+         })
+
+
+
     });
-  });
-  // hide modal
-  closeIcon.addEventListener("click", function() {
-    container.classList.remove("show");
-  });
-  // loop back
-  btnLeft.addEventListener("click", function() {
-    counter--;
-    if (counter < 0) {
-      counter = imageList.length - 1;
-    }
-    item.style.backgroundImage = `url(${imageList[counter]})`;
-  });
-  btnRight.addEventListener("click", function() {
-    counter++;
-    if (counter > imageList.length - 1) {
-      counter = 0;
-    }
-    item.style.backgroundImage = `url(${imageList[counter]})`;
-  });
+
+//Close Modal
+ closeModal.forEach((closeModal)=> {
+     closeModal.addEventListener('click', ()=>{
+            //console.log(imageSrc);
+            //console.log(imageList.indexOf("http://127.0.0.1:5500/img/cake-2.jpeg"));
+            container.classList.remove("show");
+
+     })
+ });
+
+
+ btnLeft.addEventListener('click',()=>{
+     counter--;
+     if(counter < 0){
+         counter = imageList.length - 1;
+     }
+    item.style.backgroundImage = `url('${imageList[counter]}')`;
+    console.log(counter);
+ });
+
+ btnRight.addEventListener('click',()=>{
+     console.log(counter);
+     counter++;
+     if(counter === imageList.length){
+         counter = 0;
+     }
+    item.style.backgroundImage = `url('${imageList[counter]}')`;
+ });
+
 })();
